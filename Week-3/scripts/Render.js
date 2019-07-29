fb515db19c2b50ba4f6df53131af4e82d802b69e
Aman Singh <aman_singh@epam.com>
2019-07-29 19:27:27 +0530

    did the changes and optimised dom creation

--- Week-3/scripts/Render.js
+++ Week-3/scripts/Render.js
@@ -1,41 +1,12 @@
-function setRightContent(article_data,fullContainer,container){
-  let rightContainer = document.createElement('div'),
-  categoryTitle = document.createElement('h4'),
-  select = document.createElement('select'),
-  datas=[];
-
-  rightContainer.setAttribute('class', 'right-container');
-  categoryTitle.textContent =selectCategoryText;
-  select.setAttribute('id','ddlnewschannel');
-  select.options.add( new Option(defaultSelectChannelText,null) );
-  for(item in article_data.articles){
-    if(datas.indexOf(article_data.articles[item].source.name)!==-1){
-      continue;
-    }
-    else{
-      select.options.add( new Option(article_data.articles[item].source.name,article_data.articles[item].source.name) );
-      datas.push(article_data.articles[item].source.name);
-    }
-  }
-  rightContainer.appendChild(categoryTitle);
-  rightContainer.appendChild(select);
-  fullContainer.appendChild(rightContainer);
-  select.options[0].disabled = true;
-  select.onchange = () => {
-    let val=select.value;
-    changeContent(val,fullContainer,container);
-  }
-}
 function setLeftContent(fullContainer,container){
-  let url = `https://newsapi.org/v2/top-headlines?country=in&apiKey=ac1fd7a9fc1342abb913e554a9525d85&pageSize=20`;
-  fetch(url)
-  .then((resp) => resp.json())
-  .then(function(data) {
-    fullContainer.data=data;
-    fullContent(data,container);
-    setRightContent(data,fullContainer,container);
-  })
-  .catch(function(error) {
-  });
+fetch(url)
+.then((resp) => resp.json())
+.then(function(data) {
+  fullContainer.data=data;
+  fullContent(data,container);
   fullContainer.appendChild(container);
-  }
+  setRightContent(data,fullContainer,container);
+})
+.catch(function(error) {
+});
+}
--- Week-3/scripts/Update_View.js
+++ Week-3/scripts/Update_View.js
@@ -1,104 +1,95 @@
 function changeContent(val,fullContainer,container){
-    let data=fullContainer.data;
-    let similar=0;
-    container.innerHTML = '';
-    let size=data.articles.length;
-    let card;
-    for(index in data.articles){
-      let btn = document.createElement('button'),
-      image = document.createElement('img'),
-      rightSide = document.createElement('div'),
-      title = document.createElement('h4'),
-      metaDisc = document.createElement('h6'),
-      description = document.createElement('p'),
-      horizontalLine = document.createElement('hr');
-
-      if(data.articles[index].source.name===val){
-       card= document.createElement('div');
-       card.setAttribute('class', 'box');
-       image.setAttribute('class','image');
-       image.src=data.articles[index].urlToImage;
-       rightSide.setAttribute('class','vertical');
-       title.textContent = data.articles[index].title;
-       metaDisc.setAttribute('class','metadisc');
-       metaDisc.setAttribute('id','meta');
-       metaDisc.textContent = data.articles[index].publishedAt;
-       description.setAttribute('class','description')
-       description.textContent = data.articles[index].description;
-       let content = data.articles[index].content;
-       let contentHead=data.articles[index].title;
-       if(content){
-       btn.data=content;
-       btn.heading=contentHead;
-       }
-       else{
-       btn.data=data.articles[index].description;
-       btn.heading=contentHead;
-       }
-       btn.setAttribute('id',index);
-       btn.innerHTML=buttonText;
-       btn.setAttribute('class','btncontinue');
-       rightSide.appendChild(title);
-       rightSide.appendChild(metaDisc);
-       rightSide.appendChild(description);
-       rightSide.appendChild(btn);
-       card.appendChild(image)
-       card.appendChild(rightSide)
-       container.appendChild(card);
-       if(Number(index)===Number(size-1)){
-         card.style.paddingBottom='40px';
-       }
-       container.appendChild(horizontalLine);
-       }
-    }
-   card.style.paddingBottom='40px';
+let data=fullContainer.data,
+similar=0,
+size=data.articles.length,
+card;
+container.innerHTML = '';
+for(index in data.articles){
+  if(data.articles[index].source.name===val){
+    card= document.createElement('div');
+    createLeftCard(card,data.articles[index]);
+    container.appendChild(card);
+    container.appendChild(horizontalLine);
+  }
+}
+card.style.paddingBottom='40px';
 }
 function fullContent(data,container){
-  container.innerHTML = '';
-  let size=data.articles.length;
-  for(index in data.articles ){
-      let card = document.createElement('div'),
-      image = document.createElement('img'),
-      rightSide = document.createElement('div'),
-      title = document.createElement('h4'),
-      metaDisc = document.createElement('h6'),
-      description = document.createElement('p'),
-      btn = document.createElement('button');
+container.innerHTML = '';
+let size=data.articles.length,
+card;
+for(index in data.articles ){
+  card= document.createElement('div');
+  createLeftCard(card,data.articles[index]);
+  container.appendChild(card);
+  container.appendChild(horizontalLine);
+  }
+  card.style.paddingBottom='40px';
+}
+function setRightContent(article_data,fullContainer,container){
+let rightContainer = document.createElement('div'),
+categoryTitle = document.createElement('h4'),
+select = document.createElement('select');
 
-      card.setAttribute('class', 'box');
-      image.setAttribute('class','image');
-      image.src=data.articles[index].urlToImage;
-      rightSide.setAttribute('class','vertical');
-      title.textContent = data.articles[index].title;
-      metaDisc.setAttribute('class','metadisc');
-      metaDisc.setAttribute('id','meta');
-      metaDisc.textContent = data.articles[index].publishedAt;
-      description.setAttribute('class','description')
-      description.textContent = data.articles[index].description;
-      btn.setAttribute('id',index);
-      btn.innerHTML=buttonText;
-      let content = data.articles[index].content;
-      let contentHead=data.articles[index].title;
-      if(content){
-      btn.data=content;
-      btn.heading=contentHead;
-      }
-      else{
-      btn.data=data.articles[index].description;
-      btn.heading=contentHead;
-      }
-      btn.setAttribute('class','btncontinue');
-      let horizontalLine = document.createElement('hr');
-      rightSide.appendChild(title);
-      rightSide.appendChild(metaDisc);
-      rightSide.appendChild(description);
-      rightSide.appendChild(btn);
-      card.appendChild(image)
-      card.appendChild(rightSide)
-      container.appendChild(card);
-      if(Number(index)===Number(size-1)){
-        card.style.paddingBottom='40px';
-      }
-      container.appendChild(horizontalLine);
+rightContainer.setAttribute('class', 'right-container');
+categoryTitle.textContent =selectCategoryText;
+select.setAttribute('id','ddlnewschannel');
+fillDropDownNewsChannelList(select,article_data);
+rightContainer.appendChild(categoryTitle);
+rightContainer.appendChild(select);
+fullContainer.appendChild(rightContainer);
+select.onchange = () => {
+  let val=select.value;
+  select.options.add( new Option(defaultSelectChannelText,null) );
+  changeContent(val,fullContainer,container);
+}
+}
+function createLeftCard(card,info){
+  let image = document.createElement('img'),
+  rightSide = document.createElement('div'),
+  title = document.createElement('h4'),
+  metaDisc = document.createElement('h6'),
+  description = document.createElement('p'),
+  btn = document.createElement('button')
+  content = info.content,
+  contentHead=info.title,
+  horizontalLine = document.createElement('hr');
+
+  card.setAttribute('class', 'box');
+  image.setAttribute('class','image');
+  image.src=info.urlToImage;
+  rightSide.setAttribute('class','vertical');
+  title.textContent = info.title;
+  metaDisc.setAttribute('class','metadisc');
+  metaDisc.setAttribute('id','meta');
+  metaDisc.textContent = info.publishedAt;
+  description.setAttribute('class','description')
+  description.textContent = info.description;
+  btn.setAttribute('id',index);
+  btn.innerHTML=buttonText;
+  if(content){
+  btn.data=content;
+  btn.heading=contentHead;
+  } else{
+  btn.data=info.description;
+  btn.heading=contentHead;
+  }
+  btn.setAttribute('class','btncontinue');
+  rightSide.appendChild(title);
+  rightSide.appendChild(metaDisc);
+  rightSide.appendChild(description);
+  rightSide.appendChild(btn);
+  card.appendChild(image)
+  card.appendChild(rightSide);
+}
+function fillDropDownNewsChannelList(select,article_data){
+  let datas=[];
+  for(item in article_data.articles){
+    if(datas.indexOf(article_data.articles[item].source.name)!==-1){
+      continue;
+    } else{
+      select.options.add( new Option(article_data.articles[item].source.name,article_data.articles[item].source.name) );
+      datas.push(article_data.articles[item].source.name);
     }
+  }
 }
