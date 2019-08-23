ea1401fe890a67020ed5e1e565eb79c528fa6b07
Aman Singh <aman_singh@epam.com>
2019-08-23 18:15:46 +0530

    used getters and setters

--- Week-4/ES6/subclass/subclass.challenge.js
+++ Week-4/ES6/subclass/subclass.challenge.js
@@ -32,9 +32,30 @@ class superhero {
 class Human extends superhero {
   constructor(healthpoints,city,state,...superherostuff) {
     super(...superherostuff);
-    this._healthpoints = healthpoints;
-    this.city=city;
-    this.state=state;
+    this._healthpoints=healthpoints;
+    this._city=city;
+    this._state=state;
+  }
+  get healthpoints() {
+      console.log("get healthh");
+      return this._healthpoints;
+  }
+  set healthpoints(healthpoints) {
+      this._healthpoints = healthpoints;
+  }
+  get city() {
+      console.log("get city");
+      return this.city;
+  }
+  set city(city) {
+      this.city=city;
+  }
+  get state() {
+      console.log("get sate");
+      return this._state;
+  }
+  set state(state) {
+      this._state = state;
   }
   newName() {
      this._name=`Mr ${this._name}`;
@@ -54,7 +75,7 @@ const hero3 = new Human(...details);
 console.log(hero3);
 hero3.newName();
 hero3.powerHalf();
-console.log(hero3);
+console.log(hero3.name);
 // hero3.name = "sai";
 // console.log(hero3)
 // const hero4 = new superhero(20, "abc", 8);
